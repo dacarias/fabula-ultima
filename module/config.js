f31950fa0c86fef5fccf87fedf9303a429d12699
Alessandra <alereis95@gmail.com>
2023-06-24 20:31:14 -0300

    Config

---
+++ module/config.js
@@ -0,0 +1,20 @@
+export const fabult = {};
+
+fabult.damageTypes = {
+    none: "",
+    physical: "Physical",
+    bolt: "Bolt",
+    dark: "Dark",
+    earth: "Earth",
+    fire: "Fire",
+    ice: "Ice",
+    light: "Liht",
+    poison: "Poison"
+}
+
+fabult.damageAffinities = {
+    vulnerability: "Vulnerability",
+    resistance: "Resistance",
+    immunity: "Immunity",
+    absorption: "Absorption"
+}
