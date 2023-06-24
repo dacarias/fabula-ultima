ab5f81faae39ba4de63c30cefae60c93a57a8426
aleszreis <alereis95@gmail.com>
2023-06-24 20:39:50 -0300

    added weapon and transport to template.json

---
+++ module/config.js
@@ -0,0 +1,12 @@
+export const fabult = {};
+
+fabult.attributes = {
+    "dex": "Dexterity",
+    "ins": "Insight",
+    "mig": "Might",
+    "wlp": "Willpower"
+}
+
+fabult.quality = {
+    "noQuality": "No Quality"
+}
