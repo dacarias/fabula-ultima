87b54f02b3d575a092ee6770c5a72060dba7ecf3
aleszreis <alereis95@gmail.com>
2023-06-24 21:15:40 -0300

    added a few items in config

--- module/config.js
+++ module/config.js
@@ -1,12 +1,24 @@
 export const fabult = {};
 
 fabult.attributes = {
-    "dex": "Dexterity",
-    "ins": "Insight",
-    "mig": "Might",
-    "wlp": "Willpower"
+    "dex": "fabult.attributes.dex",
+    "ins": "fabult.attributes.ins",
+    "mig": "fabult.attributes.mig",
+    "wlp": "fabult.attributes.wlp"
+}
+
+fabult.status = {
+    "dazed": "fabult.status.dazed",
+    "enraged": "fabult.status.enraged",
+    "poisoned": "fabult.status.poisoned",
+    "shaken": "fabult.status.shaken",
+    "slow": "fabult.status.slow",
+    "weak": "fabult.status.weak"
 }
 
 fabult.quality = {
-    "noQuality": "No Quality"
+    "noQuality": "fabult.quality.noQuality",
+    "minor": "fabult.quality.minor",
+    "medium": "fabult.quality.medium",
+    "major": "fabult.quality.major"
 }
