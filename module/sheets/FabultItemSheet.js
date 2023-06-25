3ab70de5a3d57990b3890a52ba1933d518d062d6
Alessandra <alereis95@gmail.com>
2023-06-25 00:07:15 -0300

    Config added

--- module/sheets/FabultItemSheet.js
+++ module/sheets/FabultItemSheet.js
@@ -2,4 +2,12 @@ export default class FabultItemSheet extends ItemSheet {
     get template() {
         return `systems/fabula-ultima/templates/sheets/${this.item.type}-sheet.html`;
     }
+
+    getData() {
+        const data = super.getData();
+
+        data.config = CONFIG.fabult;
+
+        return data;
+    }
 }
